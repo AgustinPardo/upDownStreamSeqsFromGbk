000190e66164b1c1d8fc998a3327157aedb977bd
AgustinPardo <agu_pardo@hotmail.com>
2020-11-20 15:56:00 -0300

    txt input add

--- upDownStreamSeqsFromgbk.py
+++ upDownStreamSeqsFromgbk.py
@@ -1,22 +1,36 @@
-from Bio import SeqIO
+from Bio import SeqIO, SeqFeature
 
 #Input files:
-#fasta of query  sequences
-#gbk to search the query sequences
-fasta_filename="secuencia_nucleotidos.fasta"
-gbk_filename = "st5.gbk"
+#Genbank file to search the gen positions and extended sequences.
+gbk_filename = "/home/agustin/workspace/upDownStreamSeqsFromGbk/karina/Tatroviride_IMI206040_0.gb" # <-Please change->
+input_gbk_handle  = open(gbk_filename, "r")
+
+#fasta of query sequences to take de gene id or txt with gene ids in the first colum (columns separated by \t). The first line of the txt(header of file) is avoid.
+#Include file name and "fasta" or "txt" type file.
+input_file_name_type=["/home/agustin/workspace/upDownStreamSeqsFromGbk/karina/IDcluster10.txt","txt"] # <-Please change->
 
 #Output file:
-#Fasta with the original sequences and the extended upstream and downstream sequences
-fna_filename = "300UpDownStream.fna"
-
-input_fasta_handle  = open(fasta_filename, "r")
-input_gbk_handle  = open(gbk_filename, "r")
+#Fasta with the original sequences and the extended upstream and downstream sequences. 
+fna_filename = "UpDownStream.fna" # <-Please change->
 output_handle = open(fna_filename, "w")
 
-sequence_ids=[]
-for record in SeqIO.parse(input_fasta_handle, 'fasta'):
-	sequence_ids.append(record.id)
+#How much to extend.
+upstream=1500
+downstream=0
+
+def get_ids_input(file_input, file_type):
+        input_handle  = open(file_input, "r")
+        if file_type =="fasta":
+                sequence_ids=[]
+                for record in SeqIO.parse(input_fasta_handle, 'fasta'):
+                        sequence_ids.append(record.id)
+        if file_type =="txt":
+                id_lines=input_handle.readlines()
+                sequence_ids=[x.split("\t")[0][:-3] for x in id_lines[1:]]
+        input_handle.close()
+        return sequence_ids
+
+sequence_ids = get_ids_input(input_file_name_type[0],input_file_name_type[1])
 
 for gb_record in SeqIO.parse(input_gbk_handle, "genbank"):
 	for feature in gb_record.features:
@@ -26,7 +40,6 @@ for gb_record in SeqIO.parse(input_gbk_handle, "genbank"):
 				# print(feature.location.start-feature.location.end)
 				# print (">", feature.location, gb_record.id, feature.qualifiers["locus_tag"][0])
 				# print (feature.location.extract(gb_record).seq)
-				
 				output_handle.write(">%s %s %s\n%s\n" % (
 					   feature.location,
 					   gb_record.id,
@@ -34,7 +47,7 @@ for gb_record in SeqIO.parse(input_gbk_handle, "genbank"):
 					   feature.location.extract(gb_record).seq
 					   ))
 
-				new_feature_location = SeqFeature.FeatureLocation(feature.location.start-300, feature.location.end+300, feature.location.strand)
+				new_feature_location = SeqFeature.FeatureLocation(feature.location.start-upstream, feature.location.end, feature.location.strand+downstream)
 				# print("Upstream - Downstream extended")
 				# print(new_feature_location.start-new_feature_location.end)
 				# print (">", new_feature_location, gb_record.id,feature.qualifiers["locus_tag"][0])
@@ -47,6 +60,5 @@ for gb_record in SeqIO.parse(input_gbk_handle, "genbank"):
 					   new_feature_location.extract(gb_record).seq
 					   ))
 
-input_fasta_handle.close()
 input_gbk_handle.close()
 output_handle.close()
